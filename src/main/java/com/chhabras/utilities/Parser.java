c40ce0910b5608e6cb3cf4734fd50b6a44c3a19b
Brijesh Chhabra <brijesh.chhabra@wirecard.com>
2020-12-11 22:10:03 +0100

    cleanup workspace, removed unused stuff.

--- src/main/java/com/chhabras/utilities/Parser.java
+++ src/main/java/com/chhabras/utilities/Parser.java
@@ -49,32 +49,36 @@ public class Parser {
     }
 
     private static List<String> filterList(List<String> mainList) {
-        List<String> operationalList = mainList.subList(1, endPointer(mainList));
+        int end = endPointer(mainList);
+        List<String> operationalList = mainList;
+        if(end > 0){
+            operationalList = mainList.subList(1, end);
+        }
         operationalList = operationalList.stream().filter(Parser::excludeBasedOnString).collect(Collectors.toList());
         operationalList = operationalList.stream().filter(Parser::excludeBasedONRegex).collect(Collectors.toList());
         return operationalList;
     }
 
     private static boolean excludeBasedONRegex(String s) {
-        String regex = "\\d{1,2},\\d{1,2}";
+        String regex = "\\d{1,2}(,|.)\\d{1,2}|\\d x";
         return !s.matches(regex);
     }
 
     public static boolean excludeBasedOnString(String text) {
         List<String> excludeList = new ArrayList<>();
         excludeList.add("EUR/kg");
+        excludeList.add("Tel: 089");
         excludeList.add("EUR");
         excludeList.add("Kirchheim");
         excludeList.add("85551");
         excludeList.add("Frauenhoferstr");
         excludeList.add("LIDL");
         excludeList.add("Lebensmittelspezialisten");
+        excludeList.add("Lebensmitelspeciailstent");
         excludeList.add("Fraunhoferstraße");
         excludeList.add("90129219");
         excludeList.add("edeka");
         excludeList.add("EDEKA");
-        excludeList.add("4 x");
-        excludeList.add("2 x");
         excludeList.add("kg");
         excludeList.add("Posten");
         for (String str : excludeList) {
@@ -88,7 +92,7 @@ public class Parser {
     private static int endPointer(List<String> mainList) {
         int i = 0;
         for (i = 0; i < mainList.size(); i++) {
-            if (mainList.get(i).contains("zu zahlen") | mainList.get(i).contains("SUMME")) {
+            if (mainList.get(i).contains("zu zahlen") | mainList.get(i).contains("MwStd")) {
                 break;
             }
         }
