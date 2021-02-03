f1a88e79b7c405c31414cb93198b9c5f82e8821f
Brijesh Chhabra <brijesh.chhabra@wirecard.com>
2021-02-03 21:25:55 +0100

    continue simplify parsing logic :: edeka

--- src/main/java/com/chhabras/parser/impl/EdekaParser.java
+++ src/main/java/com/chhabras/parser/impl/EdekaParser.java
@@ -1,89 +1,11 @@
 package com.chhabras.parser.impl;
 
-import com.chhabras.entities.Item;
 import com.chhabras.parser.AbstractParser;
-import com.chhabras.utilities.Regex;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class EdekaParser extends AbstractParser {
-    public static final String Edeka_regex1 = "\\d{2}.\\d{2}.\\d{4} \\d{2}:\\d{2} .*";
-    public static final String Edeka_regex2 = "\\d{2}.\\d{2}.\\d{4} \\d{2}:\\d{2} .*";
 
-    @Override
-    public int endPointer(List<String> mainList) {
-        int i = 0;
-        for (i = 0; i < mainList.size(); i++) {
-            if (mainList.get(i).contains("SUMME") | mainList.get(i).contains("MwSt")) {
-                break;
-            }
-        }
-        return i;
-    }
 
-    @Override
-    public boolean excludeBasedOnRegex(String text) {
-        String regex1 = Regex.multiplier;
-        String regex2 = Regex.price;
-        String regex3 = Regex.phone1;
-        String regex4 = Regex.postalcode;
-        if (text.matches(regex1)|| text.matches(regex2)|| text.matches(regex3)|| text.matches(regex4)) {
-            System.out.println(" ###REGREX### " + text);
-            return false;
-        }
-        return true;
-    }
 
-    @Override
-    public boolean excludeBasedOnString(String text) {
-        List<String> excludeList = new ArrayList<>();
-        excludeList.add("Fraunhoferstraße 1");
-        excludeList.add("Uhlandstr.31");
-        excludeList.add("Fraunhofer");
-        excludeList.add("Kirchheim");
-        excludeList.add("kirchheim");
-        excludeList.add("DEKA");
-        excludeList.add("EBEKA");
-        excludeList.add("EDRKA");
-        excludeList.add("Lebensmitelspeciailstent");
-        excludeList.add("edeka");
-        excludeList.add("EUR");
-        excludeList.add("Posten:");
-        excludeList.add("KASSE:");
-        excludeList.add("MARKT:");
-        for (String str : excludeList) {
-            if (text.contains(str)) {
-                System.out.println(" ###STRING### " + text);
-                return false;
-            }
-        }
-        return true;
-    }
-
-    @Override
-    public boolean validate(List<Item> items) {
-        List<Boolean> results = new ArrayList<>();
-        for (Item item : items) {
-            if(item.getPrice() == null){
-                results.add(false);
-            }
-        }
-        if (results.contains(false)) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    @Override
-    public String refinePrice(String text) {
-        if (text.matches(Regex.refine_price2)) {
-            return text.split("\\s")[0];
-        }
-        if (text.matches(Regex.refine_price3)) {
-            return text.split("\\*")[0];
-        }
-        return text;
-    }
 }
--- src/test/java/TestEdeka.java
+++ src/test/java/TestEdeka.java
@@ -2,6 +2,9 @@ import com.chhabras.parser.impl.EdekaParser;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static org.testng.Assert.assertTrue;
 import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertFalse;
@@ -27,6 +30,20 @@ public class TestEdeka {
         assertEquals("6,21", parser.refinePrice(input3));
         assertEquals("0,15", parser.refinePrice(input4));
         assertEquals("1,35", parser.refinePrice(input5));
+    }
+
+    @Test
+    public void test_1() {
+        String input1 = "ZONIN CHIANTI0,75L 8,76 B";
+        String input2 = "SUMME EUR 8.76";
+        String input3 = "SOMAT TABS 7,69 B : ";
+
+        assertEquals("ZONIN CHIANTI", parser.segregate(input1).get("description").trim());
+        assertEquals("8,76", parser.segregate(input1).get("price").trim());
+        assertEquals("SUMME EUR", parser.segregate(input2).get("description").trim());
+        assertEquals("8.76", parser.segregate(input2).get("price").trim());
+        assertEquals("SOMAT TABS", parser.segregate(input3).get("description").trim());
+        assertEquals("7,69", parser.segregate(input3).get("price").trim());
     }
 
 }
